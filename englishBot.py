b869bbaf068c363a0c7e7be1f153c0a8ff5ba456
daily sergey <guskov13sv@gmail.com>
2020-02-17 00:51:22 +0300

    added api google translator and word random

--- englishBot.py
+++ englishBot.py
@@ -1,9 +1,22 @@
-import key
 import telebot
+import wordAPI
+import key
+import json
 
+# Initialize
 bot = telebot.TeleBot(key.API_skipper)
-#keyboard1 = telebot.types.ReplyKeyboardMarkup(row_width=1)
-#keyboard1.row('Давай новое слово')
+
+keyboard1 = telebot.types.ReplyKeyboardMarkup(row_width=1)
+keyboard1.row('Давай New English word')
+
+
+# -1. Добавить логирование
+# 0. Обрамить try catch
+# 1. Найти API для получения английского слова - done
+# 2. Выдавать новое слово по запросу пользователя
+# 3. Отправлять пользователю уведомления на экран
+# 4. Хранить chat_id
+# 5. Если уже пользователь вводил /start, говорить ему об этом
 
 
 @bot.message_handler(commands=['start'])
@@ -15,12 +28,22 @@ def start_message(message):
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, 'Привет, мой создатель')
-    elif message.text.lower() == 'пока':
-        bot.send_message(message.chat.id, 'Прощай, создатель')
-    elif message.text.lower() == 'я тебя люблю':
-        bot.send_sticker(message.chat.id, 'CAADAgADZgkAAnlc4gmfCor5YbYYRAI')
+    if message.text.lower() == 'word':
+        generateEngWord(message)
+
+# Refactored func with one transaction of word
+
+
+def generateEngWord(message):
+    api = wordAPI.engWord()
+    resultEngWord = api.getRandomWord()
+    data = json.loads(resultEngWord)
+
+    engWord = data['word']
+    translation = api.getTranslation(engWord)
+
+    bot.send_message(message.chat.id, engWord)
+    bot.send_message(message.chat.id, translation)
 
 
 @bot.message_handler(content_types=['sticker'])
---
+++ wordAPI.py
@@ -0,0 +1,53 @@
+import requests
+import key
+import json
+
+
+class engWord:
+    # Constructor
+    def __init__(self):
+        self.ResultLang = 'ru'
+        self.url = "https://wordsapiv1.p.rapidapi.com/words/"
+        self.headers = {'x-rapidapi-host': "wordsapiv1.p.rapidapi.com",
+                        'x-rapidapi-key': key.WordApi}
+
+    def getRandomWord(self):
+        url = "https://wordsapiv1.p.rapidapi.com/words/"
+        headers = {'x-rapidapi-host': "wordsapiv1.p.rapidapi.com",
+                   'x-rapidapi-key': key.WordApi}
+        querystring = {"random": "true"}
+        response = requests.request(
+            "GET", url, headers=headers, params=querystring)
+        print(response.text)
+        return response.text
+
+    def getTranslation(self, word):
+        try:
+            # GOOGLE TRANSLATION API
+            url = "https://translation.googleapis.com/language/translate/v2"
+            payload = "q={}&target={}&key={}".format(
+                word, self.ResultLang, key.GoogleTranslationKey)
+            headers = {'content-type': "application/x-www-form-urlencoded"}
+            print(url, payload)
+            response = requests.request(
+                "POST", url, data=payload, headers=headers)
+            result = json.loads(response.text)[
+                'data']['translations'][0]['translatedText'].lower()
+
+            return result
+        except Exception as er:
+            print(er)
+            return "Возникла ошибка при переводе"
+
+# VERSION OF https://rapidapi.com/gofitech/api/nlp-translation/endpoints
+#        url = "https://nlp-translation.p.rapidapi.com/v1/translate"
+# Forming query_word and config FROM and TO langs
+#        payload = "from=en&text={}&to=ru".format(word)
+#        headers = {
+#            'x-rapidapi-host': "nlp-translation.p.rapidapi.com",
+#            'x-rapidapi-key': "55189bdbb6msha49302ebdb3d880p17ce4bjsn1a825f8d4d7a",
+#            'content-type': "application/x-www-form-urlencoded"
+#        }
+#        response = requests.request("POST", url, data=payload, headers=headers)
+#res = self.translator.translate(word)
+# print(self.translator.translate(word))
