01294e147b7ae53a60c1a17446b8ef5cee63b6fa
daily sergey <guskov13sv@gmail.com>
2020-02-16 20:32:24 +0300

    get first instructs from habr paper

---
+++ englishBot.py
@@ -0,0 +1,31 @@
+import key
+import telebot
+
+bot = telebot.TeleBot(key.API_skipper)
+#keyboard1 = telebot.types.ReplyKeyboardMarkup(row_width=1)
+#keyboard1.row('Давай новое слово')
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    print(message)
+    bot.send_message(
+        message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
+
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
+    elif message.text.lower() == 'я тебя люблю':
+        bot.send_sticker(message.chat.id, 'CAADAgADZgkAAnlc4gmfCor5YbYYRAI')
+
+
+@bot.message_handler(content_types=['sticker'])
+def sticker_id(message):
+    print(message)
+
+
+bot.polling()
