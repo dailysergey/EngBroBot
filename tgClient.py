d8bbad0f11739aa10fc1529ff6166ffad30c31e9
daily sergey <guskov13sv@gmail.com>
2020-02-20 23:27:52 +0300

    configured and checked connection to mongo docker

---
+++ tgClient.py
@@ -0,0 +1,17 @@
+import key
+from pymongo import MongoClient
+# pprint library is used to make the output look more pretty
+from pprint import pprint
+
+
+class MongoEntity:
+
+    def __init__(self):
+        try:
+            client = MongoClient(key.MONGO_URL)
+            db = client['tgclients']
+            tgCol = db['clients']
+            serverStatusResult = db.command("serverStatus")
+            pprint(serverStatusResult)
+        except Exception as ex:
+            pprint(ex)
