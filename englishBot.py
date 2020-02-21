4b282dbcfee2fb94aed7e53e11f09ae95270b904
daily sergey <guskov13sv@gmail.com>
2020-02-21 07:17:04 +0300

    checked user 'start' message first or more time

--- englishBot.py
+++ englishBot.py
@@ -3,7 +3,7 @@ import wordAPI
 import key
 import json
 from pprint import pprint
-
+import tgClient
 # Initialize
 bot = telebot.TeleBot(key.API_skipper)
 
@@ -25,8 +25,22 @@ def start_message(message):
     # TODO Start store history with user here to mongo
     # message.json["from"] - info about user from whom message
     # message.json["text"] - actual text from user
-    bot.send_message(
-        message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
+    db = tgClient.MongoEntity().connect
+    clients = db['clients']
+    messages = db['message']
+    user_id = message.json['from']['id']
+
+    if clients.find({'id': user_id}).count() > 0:
+        bot.send_message(
+            message.chat.id, 'Дорогой, ты уже это мне писал ', reply_markup=keyboard1)
+    else:
+        clients.insert_one(message.json['from'])
+        text_dict = {'message_id': message.json['message_id'], 'user_id': user_id,
+                     'date': message.json['date'], 'text': message.json['text']}
+        messages.insert_one(text_dict)
+        bot.send_message(
+            message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
+# TODO create func with norm deserialize from tg_object
 
 
 @bot.message_handler(content_types=['text'])
