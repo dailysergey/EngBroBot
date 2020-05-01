b204f295b7f49782115ed907d6410808b4b9f67a
daily sergey <guskov13sv@gmail.com>
2020-05-01 17:00:23 +0300

    set to one name convention

--- src/engbrobot.py
+++ src/engbrobot.py
@@ -42,9 +42,9 @@ def start_message(message):
         logging.info('start_message:From {}.Text:{}'.format(
             message.chat.id, message.json['text']))
         # get user_id
-        user_id = message.json['from']['id']
+        userId = message.json['from']['id']
         # check if user already send me 'START' message
-        if clients.find({'id': user_id}).count() > 0:
+        if clients.find({'id': userId}).count() > 0:
             logging.info('start_message: Hello again block')
 
             bot.send_sticker(
@@ -58,7 +58,7 @@ def start_message(message):
             user['send_notifies'] = 'false'
             clients.insert_one(user)
             # form message from user
-            text_dict = {'message_id': message.json['message_id'], 'user_id': user_id,
+            text_dict = {'message_id': message.json['message_id'], 'user_id': userId,
                          'date': message.json['date'], 'text': message.json['text']}
             # store messages from user
             messages.insert_one(text_dict)
@@ -86,30 +86,37 @@ def help_message(message):
 
 @bot.message_handler(commands=['stop'])
 def stop_message(message):
-    # TODO create column in table isStopped
-    pass
+    userId = message.json['from']['id']
+    clients.update({'id': userId}, {"$set": {'send_notifies': 'false'}},
+                   upsert=True)
+    # send message to say goodbye
+    bot.send_sticker(
+        message.chat.id, botMessages.sticker_notify_goodbye)
+    bot.send_message(
+        message.chat.id, botMessages.notify_goodbye, reply_markup=kb.keyboard1)
 
 
 @bot.message_handler(content_types=['photo'])
 def send_media(message):
     try:
-        execution_path = os.getcwd()
-        image = bot.get_file(message.photo[len(message.photo)-1].file_id)
+        currentDir = os.getcwd()
+        inputImage = bot.get_file(message.photo[len(message.photo)-1].file_id)
+        # name of input image
         photoName = "{}.jpg".format(message.chat.id)
-        photo_path = os.path.join(
-            execution_path, "src", "objectDetection", "input", photoName)
+        destinationInputPhotoPath = os.path.join(
+            currentDir, "src", "objectDetection", "input", photoName)
 
-        downloaded_image = bot.download_file(image.file_path)
+        downloadedImage = bot.download_file(inputImage.file_path)
 
-        with open(photo_path, 'wb') as new_file:
-            new_file.write(downloaded_image)
-        logging.info('Saved photo to {}'.format(photo_path))
+        with open(destinationInputPhotoPath, 'wb') as new_file:
+            new_file.write(downloadedImage)
+
+        logging.info('Saved photo to {}'.format(destinationInputPhotoPath))
         resultImage = imageAI.detect(photoName)
         logging.info('Sending photo to {} with photo from {}'.format(
             message.chat.id, resultImage))
         bot.send_photo(message.chat.id, photo=open(resultImage, "rb"))
     except Exception as ex:
-        print(ex)
         logging.error('[Send media(photo)]: Error {}.'.format(ex))
 
 
@@ -122,7 +129,7 @@ def handle_query(call):
             bot.edit_message_text(chat_id=call.message.chat.id,
                                   text=call.message.text,
                                   message_id=call.message.message_id,
-                                  reply_markup=handlers.rateKeyboard(True),
+                                  reply_markup=kb.rateKeyboard(True),
                                   parse_mode='HTML')
 
         if 'no' in call.data:
@@ -131,29 +138,25 @@ def handle_query(call):
             bot.edit_message_text(chat_id=call.message.chat.id,
                                   text=call.message.text,
                                   message_id=call.message.message_id,
-                                  reply_markup=handlers.rateKeyboard(False),
+                                  reply_markup=kb.rateKeyboard(False),
                                   parse_mode='HTML')
     except Exception as ex:
         print(ex)
         logging.error('[Handle_query]: Error {}.'.format(ex))
-
-#    bot.answer_callback_query(callback_query_id=call.id,
-#                                show_alert=True,
-#                                text="You Clicked {} ".format(call.data))
 
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     # get user_id
     api = wordAPI.engWord()
-    user_id = message.json['from']['id']
+    userId = message.json['from']['id']
     if message.text == botMessages.keyboard_hello_row1:
-        handlers.teachNewEnglishWord(api, user_id, bot, clients)
+        handlers.teachNewEnglishWord(api, userId, bot, clients)
     elif message.text == botMessages.keyboard_enable_noty_row3:
         # how to ask questions
-        clients.update({'id': user_id}, {"$set": {'send_notifies': 'true'}},
+        clients.update({'id': userId}, {"$set": {'send_notifies': 'true'}},
                        upsert=True)
-        clients.update({'id': user_id}, {"$set": {'counter': 1}},
+        clients.update({'id': userId}, {"$set": {'counter': 1}},
                        upsert=True)
         # send message to congrat
         bot.send_sticker(
@@ -163,7 +166,7 @@ def send_text(message):
 
     elif message.text == botMessages.keyboard_disable_noty_row3:
 
-        clients.update({'id': user_id}, {"$set": {'send_notifies': 'false'}},
+        clients.update({'id': userId}, {"$set": {'send_notifies': 'false'}},
                        upsert=True)
         # send message to say goodbye
         bot.send_sticker(
@@ -177,21 +180,21 @@ def send_text(message):
             message.chat.id, botMessages.topic_reply, reply_markup=kb.keyboard3)
     # region TOPIC
     elif message.text in botMessages.topics:
-        clients.update({'id': user_id}, {"$set": {'topic': message.text, 'counter': 1}},
+        clients.update({'id': userId}, {"$set": {'topic': message.text, 'counter': 1}},
                        upsert=True)
-        if user_id == int(key.adminKey):
-            bot.send_message(user_id, botMessages.success_set_topic +
+        if userId == int(key.adminKey):
+            bot.send_message(userId, botMessages.success_set_topic +
                              message.text, reply_markup=kb.keyboard4)
         else:
-            bot.send_message(user_id, botMessages.success_set_topic +
+            bot.send_message(userId, botMessages.success_set_topic +
                              message.text, reply_markup=kb.keyboard2)
-    elif user_id == int(key.adminKey) and message.text == botMessages.send_everybody:
+    elif userId == int(key.adminKey) and message.text == botMessages.send_everybody:
         handlers.autoResendMessages(bot, clients)
-    elif user_id == int(key.adminKey) and message.text == botMessages.get_stats:
+    elif userId == int(key.adminKey) and message.text == botMessages.get_stats:
         for user in clients.find():
             for sms_stat in messages.find(filter={"user_id": user["id"]}):
                 userInfo = handlers.getUsersInfo(user, sms_stat)
-                bot.send_message(user_id, userInfo)
+                bot.send_message(userId, userInfo)
 
     else:
         # TODO translate text
@@ -205,10 +208,13 @@ updater = Updater(key.API_skipper, use_context=True)
 job = updater.job_queue
 
 
-def callback_minute(context: telegram.ext.CallbackContext):
-    handlers.autoResendMessages(context.bot, clients)
+def callback_resender(context: telegram.ext.CallbackContext):
+    hour = datetime.datetime.now().hour
+    if hour < 20 and hour > 10:
+        handlers.autoResendMessages(context.bot, clients)
 
 
-job.run_repeating(callback_minute, interval=60, first=0)
+job.run_repeating(
+    callback_resender, interval=datetime.timedelta(hours=2), first=0)
 job.start()
 bot.polling()
